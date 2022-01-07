b2f67aa41c7d44b43702908f40a68228526a3cbe
oksanad <oksanad27.04@gmail>
2022-01-07 23:54:18 +0200

    delete incorrect models, add app and crate models correctly

--- mycrudApp/models.py
+++ mycrudApp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.urls import reverse
 
 class Driver(models.Model):
-    id = models.IntegerField(max_length=20, unique=True)
+    id = models.IntegerField(unique=True, primary_key=True)
     first_name = models.CharField(max_length=250)
     second_name = models.CharField(max_length=250)
     created_at = models.DateField(auto_now_add=True)
@@ -10,7 +10,7 @@ class Driver(models.Model):
 
 
 class Vehicle(models.Model):
-    id = models.IntegerField(max_length=20, unique=True)
+    id = models.IntegerField(unique=True, primary_key=True)
     make = models.CharField(max_length=250)
     model = models.CharField(max_length=250)
     plate_number = models.CharField(max_length=250) # "AA 1234 OO"
